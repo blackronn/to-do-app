15b113574172f17fa5b18159a0ddf305de506f88
blackronn <200101057@ogrenci.yalova.edu.tr>
2025-08-07 14:41:00 +0300

    Add User entity, controller, and repository classes

--- src/main/java/com/todoapp/todo/TodoApplication.java
+++ src/main/java/com/todoapp/todo/TodoApplication.java
@@ -2,8 +2,14 @@ package com.todoapp.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.todoapp"})
+@EntityScan(basePackages = "com.todoapp")
+@EnableJpaRepositories(basePackages = "com.todoapp")
 public class TodoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/todoapp/todo/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.todoapp.todo.controller;
+
+import com.todoapp.todo.model.User;
+import com.todoapp.todo.service.abstracts.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user/")
+public class UserController {
+    @Autowired
+    private IUserService userService;
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+    @PostMapping("/login")
+    public User login(@RequestBody User data){
+        return userService.login(data.getUsername(),data.getPassword());
+    }
+    @GetMapping("/list")
+    public List<User> getAllUsers() {
+        return userService.getAllUser();
+    }
+
+}
